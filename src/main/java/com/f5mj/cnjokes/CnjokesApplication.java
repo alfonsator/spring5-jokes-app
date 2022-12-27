4ba83878f45b5356b12ad5fb06f6ce3be3fde22a
alfonsator <64138333+alfonsator@users.noreply.github.com>
2022-12-27 18:17:41 +0100

    Spring Initializr project add

---
+++ src/main/java/com/f5mj/cnjokes/CnjokesApplication.java
@@ -0,0 +1,13 @@
+package com.f5mj.cnjokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnjokesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CnjokesApplication.class, args);
+	}
+
+}
